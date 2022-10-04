96b4db85ebc052962eb6c051aa05860565c5df0b
AbyanRamzi <abyanramzi311000@gmail.com>
2022-10-04 15:39:14 +0700

    Update

---
+++ src/main/kotlin/Challenge.kt
@@ -0,0 +1,4 @@
+fun main () {
+    val lamaParkir = 9
+
+}
---
+++ src/main/kotlin/Operations.kt
@@ -0,0 +1,8 @@
+fun main () {
+    val total = 1+2
+    println(total)
+
+    val isTrue = true
+    val isFalse = false
+    println(isTrue && isFalse)
+}
